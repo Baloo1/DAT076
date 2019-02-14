e316b7d905f1017ce26f380659784d28c62cdec3
jonathangildevall <jonathan.gildevall@gmail.com>
2019-02-14 17:59:46 +0100

    Added a test navbar!

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,24 @@
-function Home() {
-  return <div>Welcome to next.js!</div>
-}
+import React from "react";
+import {Navbar} from "react-bootstrap"
+import Nav from "react-bootstrap/Nav";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Form from "react-bootstrap/Form";
+import FormControl from "react-bootstrap/FormControl";
+import Button from "react-bootstrap/Button";
+import TempNavBar from "./navbar";
 
+function Home() {
+
+  return (
+      <div>
+        <link
+          rel="stylesheet"
+          href="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css"
+          integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS"
+          crossOrigin="anonymous"
+        />
+        <TempNavBar/>
+      </div>
+  )
+}
 export default Home
