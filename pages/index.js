968ed7141035650f0448cdc6ad86f846aa30a633
Johannes Mattsson <karlmattsson96@gmail.com>
2019-02-14 18:16:17 +0100

    Put boostrap css in <Head>

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import {Navbar} from "react-bootstrap"
+import Head from 'next/head';
+
 import Nav from "react-bootstrap/Nav";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import Form from "react-bootstrap/Form";
@@ -11,12 +13,15 @@ function Home() {
 
   return (
       <div>
-        <link
-          rel="stylesheet"
-          href="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css"
-          integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS"
-          crossOrigin="anonymous"
-        />
+          <Head>
+            <title>DAT076</title>
+          <link
+            rel="stylesheet"
+            href="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css"
+            integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS"
+            crossOrigin="anonymous"
+          />
+        </Head>
         <TempNavBar/>
       </div>
   )
