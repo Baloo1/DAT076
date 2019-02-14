1c06e48ac14778e9c255b8ec78c39d587d99bcfa
Johannes Mattsson <karlmattsson96@gmail.com>
2019-02-14 18:46:20 +0100

    Added linting and cleaned repo

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,23 @@
-import React from "react";
-import {Navbar} from "react-bootstrap"
+import React from 'react';
 import Head from 'next/head';
 
-import Nav from "react-bootstrap/Nav";
-import NavDropdown from "react-bootstrap/NavDropdown";
-import Form from "react-bootstrap/Form";
-import FormControl from "react-bootstrap/FormControl";
-import Button from "react-bootstrap/Button";
-import TempNavBar from "./navbar";
+import TempNavBar from './Navbar';
 
 function Home() {
 
   return (
-      <div>
-          <Head>
-            <title>DAT076</title>
-          <link
-            rel="stylesheet"
-            href="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css"
-            integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS"
-            crossOrigin="anonymous"
-          />
-        </Head>
-        <TempNavBar/>
-      </div>
-  )
+    <div>
+      <Head>
+        <title>DAT076</title>
+        <link
+          rel="stylesheet"
+          href="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css"
+          integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS"
+          crossOrigin="anonymous"
+        />
+      </Head>
+      <TempNavBar/>
+    </div>
+  );
 }
-export default Home
+export default Home;
--- server/server.js
+++ server/server.js
@@ -1,27 +1,27 @@
-const express = require('express')
-const next = require('next')
-const bodyParser = require('body-parser')
+const express = require('express');
+const next = require('next');
+const bodyParser = require('body-parser');
 
-const PORT = process.env.PORT || 3000
-const dev = process.env.NODE_DEV !== 'production'
-const nextApp = next({ dev })
-const handle = nextApp.getRequestHandler()
+const PORT = process.env.PORT || 3000;
+const dev = process.env.NODE_DEV !== 'production';
+const nextApp = next({ dev });
+const handle = nextApp.getRequestHandler();
 
 nextApp.prepare().then(() => {
-    // express code here
-    const app = express()
-    app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({ extended: true }));
+  // express code here
+  const app = express();
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
 
-    // API routes
-    app.use('/api/', require('./routes/index'))
+  // API routes
+  app.use('/api/', require('./routes/api'));
 
-    // React routes
-    app.get('*', (req,res) => {
-        return handle(req,res)
-    })
-    app.listen(PORT, err => {
-        if (err) throw err;
-        console.log(`ready at http://localhost:${PORT}`)
-    })
-})
+  // React routes
+  app.get('*', (req,res) => {
+    return handle(req,res);
+  });
+  app.listen(PORT, err => {
+    if (err) throw err;
+    console.log(`ready at http://localhost:${PORT}`);
+  });
+});
