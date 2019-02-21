97cc95671b444dfac9264321fcbc8fa46acd2c4d
Johannes Mattsson <karlmattsson96@gmail.com>
2019-02-21 16:41:12 +0100

    Start of login

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const db = requre('./queries')
+// const db = require('./queries');
 
 // API Routes
 router.get('/', (req, res) => {
   res.send('hello');
 });
 
-router.get('/login', db.validatePassword);
+// router.get('/login', db.validatePassword);
 
 
 
