ace88e7ec5586b3bfb034cffa23b42da56b31544
Viktor Lindblom <viktor.lindblom@gmail.com>
2019-02-24 19:38:29 +0100

    added some routers for getting / uploading user info

--- server/migrations/20190221161722_users.js
+++ server/migrations/20190221161722_users.js
@@ -3,7 +3,7 @@ exports.up = function(knex, Promise) {
     return Promise.all([
         knex.schema.createTable('users', table => {
             table.increments('id').primary();
-            table.string('email').notNullable();
+            table.string('email').notNullable().unique();
             table.string('password').notNullable();
             table.enum('role', ['user', 'admin']).defaultTo('user');
             table.string('name');
--- server/routes/api.js
+++ server/routes/api.js
@@ -1,19 +1,60 @@
 const express = require('express');
 const router = express.Router();
+const {User, Experience, About, Project} = require('../models/schema')
 
 // API Routes
 router.get('/', (req, res) => {
   res.send('hello');
 });
 
+
 router.get('/users', async (req, res) => {
   const users = await User.query()
   res.json(users)
 });
 
-router.get('/experiences/:id', async (req, res) => {
-  const user = await User.query().findById(req.params.id).eager('user_experiences')
-  res.json(user)
+// Get all experiences, abouts, projects of a user by id
+router.get('/:id/experiences', async (req, res) => {
+  const experiences = await Experience.query().where('user_id', '=', req.params.id)
+  res.json(experiences)
 });
+
+router.get('/:id/abouts', async (req, res) => {
+  const abouts = await About.query().where('user_id', '=', req.params.id)
+  res.json(abouts)
+});
+
+router.get('/:id/projects', async (req, res) => {
+  const projects = await Project.query().where('user_id', '=', req.params.id)
+  res.json(projects)
+});
+
+//TODO add a middleware function to check if user logged in is same as trying to add
+
+// Add a new experience, about or project of a user by id
+router.post('/:id/experience', async (req, res) => {
+  const newExperience = req.body
+
+  const experience = await Experience.query().insert(newExperience)
+
+  res.send(experience)
+});
+
+router.post('/:id/about', async (req, res) => {
+  const newAbout = req.body
+
+  const ebout = await Experience.query().insert(newAbout)
+
+  res.send(newAbout)
+});
+
+router.post('/:id/project', async (req, res) => {
+  const newProject = req.body
+
+  const project = await Experience.query().insert(newProject)
+
+  res.send(project)
+});
+
 
 module.exports = router;
