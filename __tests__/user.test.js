6c68d13670988b3695fe43bfc298921163c3292b
Viktor Lindblom <viktor.lindblom@gmail.com>
2019-03-17 20:28:00 +0100

    Fixed so we get correct array / not array for the test

--- __tests__/user.test.js
+++ __tests__/user.test.js
@@ -1,42 +1,16 @@
+import 'jsdom-global/register';
 import React from 'react';
 import User from '../pages/user';
 import Adapter from 'enzyme-adapter-react-16';
-import { shallow, configure } from 'enzyme';
+import { waitForState } from 'enzyme-async-helpers';
+import { shallow, configure, mount } from 'enzyme';
 
 configure({adapter: new Adapter()});
 
 jest.mock('axios', () => {
-    const exampleUser = [
+    const exampleUser =
         {
-            email: 'user@example.com',
-            id: 0,
-            image_id: 0,
-            name: 'user',
-            phone: '12345',
-            role: 'user',
-            twitter: '@user',
-            website: 'www.example.com'
-        }
-    ];
-
-    return {
-        get: jest.fn(() => {
-            return Promise.resolve(exampleUser);}
-        ),
-    };
-});
-
-const axios = require('axios');
-
-it('fetch user on #componentDidMount', async (done) => {
-
-    const user = shallow(<User />);
-    user
-        .instance()
-        .componentDidMount()
-        .then(() => {
-            expect(axios.get).toHaveBeenCalled();
-            expect(user.state()).toHaveProperty('user', [
+            data:
                 {
                     email: 'user@example.com',
                     id: 0,
@@ -47,7 +21,49 @@ it('fetch user on #componentDidMount', async (done) => {
                     twitter: '@user',
                     website: 'www.example.com'
                 }
-            ]);
+        };
+    const exArray = {
+        data: [
+            {
+                title: 'A title',
+                description: 'A description'
+            }
+        ]
+    };
+
+    return {
+        get: jest.fn((text) => {
+            if(text.includes('user/undefined/')) {
+                return Promise.resolve(exArray);
+            } else {
+                return Promise.resolve(exampleUser);}
+            }
+        ),
+    };
+});
+
+const axios = require('axios');
+
+it('fetch user on #componentDidMount', async () => {
+
+    const user = shallow(<User />);
+    user
+        .instance()
+        .componentDidMount()
+        .then(() => {
+            expect(axios.get).toHaveBeenCalled();
+            expect(user.state()).toHaveProperty('user',
+                {
+                    email: 'user@example.com',
+                    id: 0,
+                    image_id: 0,
+                    name: 'user',
+                    phone: '12345',
+                    role: 'user',
+                    twitter: '@user',
+                    website: 'www.example.com'
+                }
+            );
             done();
         });
 });
